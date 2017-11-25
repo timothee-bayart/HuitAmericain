7b710c6168d744ab9329d156b59c20d6c74d3e11
idrirap <idrirap@hotmail.fr>
2017-11-25 19:55:55 +0100

    séparartion du main dans une classe specifique

---
+++ src/bayart/ayouaz/huit/americain/Model/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bayart.ayouaz.huit.americain.Model;
+
+/**
+ *
+ * @author ed
+ */
+public class Main {
+    
+}
