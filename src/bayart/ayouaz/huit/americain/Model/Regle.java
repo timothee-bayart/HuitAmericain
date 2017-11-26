1edfd55b15baf6b2869d0bf9dc39ddfa9773699e
idrirap <idrirap@hotmail.fr>
2017-11-26 14:17:34 +0100

    Suppression de javadoc inutile suite au precedent commit

--- src/bayart/ayouaz/huit/americain/Model/Regle.java
+++ src/bayart/ayouaz/huit/americain/Model/Regle.java
@@ -11,16 +11,6 @@ public abstract class Regle {
 
     public abstract boolean aGagne(GroupeDeCarte paquetDuJoueurEnCours);
     
-    /**
- * 0 rien
- * -1 inverser sens du jeu
- * -2 passer tour
- * -3 rejouer
- * -4 décider de la prochaine couleur
- * -5 designe le prochain joueur ou echange ses cartes avec lui
- *  valeurs positives : nombre de carte a piocher pour l'adversaire
- * @return la valeur code qui donne les malus infligeable à l'adversaire 
- */
     public abstract void leProchainJoueurDevra(Partie p);
 
 }
