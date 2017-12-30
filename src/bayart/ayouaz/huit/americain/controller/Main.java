77a235c907343dc30e189f80268e6c9284d7f7cd
idrirap <idrirap@hotmail.fr>
2017-12-30 20:12:27 +0100

    ajout d'une fenetre. Je n'ai pas encore fair les controllers

--- src/bayart/ayouaz/huit/americain/controller/Main.java
+++ src/bayart/ayouaz/huit/americain/controller/Main.java
@@ -5,6 +5,8 @@
  */
 package bayart.ayouaz.huit.americain.controller;
 
+import bayart.ayouaz.huit.americain.view.ViewGraphic;
+
 /**
  *
  * @author ed
@@ -12,6 +14,8 @@ package bayart.ayouaz.huit.americain.controller;
 public class Main {
     
     public static void main(String[] args) {
+        
+                ViewGraphic vg = new ViewGraphic();
 		Partie partie = new Partie();
 		partie.initParamsDuJeu();
 		partie.demarrerJeu();
--- src/bayart/ayouaz/huit/americain/view/ViewGraphic.java
+++ src/bayart/ayouaz/huit/americain/view/ViewGraphic.java
@@ -1,11 +1,47 @@
 package bayart.ayouaz.huit.americain.view;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class ViewGraphic  implements Observer {
-    @Override
+public class ViewGraphic extends JFrame implements Observer {
+    private ArrayList<JButton> menu;
+    private JPanel panel;
+    private GridBagConstraints constraint;
+    @Override 
     public void update(Observable o, Object arg) {
 
+    }
+    
+    public ViewGraphic(){
+        this.setTitle("Huit Americain");
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menu = new ArrayList<JButton>();
+        init();
+    }
+    
+    private void init(){
+        menu.add(new JButton("Démarrer la partie"));
+       	menu.add(new JButton("Ajouter un joueur"));
+       	menu.add(new JButton("Choisir la variante"));
+       	menu.add(new JButton("Quitter"));
+        panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        constraint = new GridBagConstraints();
+        constraint.fill = GridBagConstraints.BOTH;
+        
+        constraint.gridx=0;
+        for(int i=0;i<menu.size();++i){
+            constraint.gridy=i;
+            panel.add(menu.get(i), constraint);
+        }
+        this.setContentPane(panel);
+        this.pack();
     }
 }
